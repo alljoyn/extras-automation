1ac8f6cd57802327a8283efd6460a47c6ab19b83
Tony Wallace <awallace@qce.qualcomm.com>
2015-04-07 16:57:10 -0700

    updates for r15.04
    Change-Id: I09f7cea86cc59c5e05a867b323296c363d8fffa8
    Signed-off-by: Tony Wallace <awallace@qce.qualcomm.com>

--- jenkins/genversion.py
+++ jenkins/genversion.py
@@ -1,4 +1,5 @@
-# Copyright (c) AllSeen Alliance. All rights reserved.
+
+# Copyright AllSeen Alliance. All rights reserved.
 #
 #    Permission to use, copy, modify, and/or distribute this software for any
 #    purpose with or without fee is hereby granted, provided that the above
@@ -11,7 +12,6 @@
 #    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 #    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 #    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
 
 import re
 import sys
