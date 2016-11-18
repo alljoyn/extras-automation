d317fc95bf4a99aab35af17c7599e5175424f198
C.J. Collier <cjcollier@linuxfoundation.org>
2016-11-18 13:04:12 -0800

    Develop script to automate license updates
    * README describes in prose the purpose of this script
    * bin/setup configures the system for the script to run
    
    * bin/do-relicense creates a draft change for each branch of each
      project which contain any files matching the "old license" regex
        /Copyright(.*)AllSeen Alliance. All rights reserved./
      It then replaces the "old license" regex with the content of the
      template/new_license.txt template file with variable interpolation,
      if necessary.  The commit message used is gathered from
      template/new_license.txt.
    
    * bin/add-reviewers.pl queries gerrit for project committers and adds
      them to the appropriate drafts created by bin/do-relicense
    
    * bin/promote-draft.pl promotes all open draft code reviews with topic
      matching regular expression /^.*_re-license/
    
    * bin/abandon-reviews.pl abandons all open reviews with topic matching
      regular expression /^.*_re-license/
    
    * new license from tempalte/new_license.txt
    * new Microsoft license from tempalte/new_ms_license.txt
    * commit message template in template/commit-message.txt
    * Apache v2.0 license added as LICENSE-2.0 from template/LICENSE-2.0
    
    /*
     * Microsoft licenses update completed successfully
     */
    
    Change-Id: I020f63576a71f0171b5a37dac2b806a33ea442a9
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ relicense/bin/promote-draft.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+use strict;
+
+use JSON::XS;
+use Data::Dumper;
+
+my $DEBUG=1;
+
+my $json = JSON::XS->new->utf8->pretty;
+
+my $domain = q{allseenalliance.org};
+
+my $gerrit_host = qq{git.$domain};
+my $gerrit_port = 29418;
+
+# Query gerrit for all re-license changes
+print STDERR 'fetching list of changes with topic matching re-license...' if $DEBUG;
+my $gerrit_cmd = qq{ssh -p ${gerrit_port} ${gerrit_host} gerrit query --format=JSON topic:^.*_re-license --current-patch-set};
+my( @gerrit_output ) = qx{$gerrit_cmd};
+print STDERR "done$/" if $DEBUG;
+
+my $gerrit_status = $json->decode( pop( @gerrit_output ) );
+
+my( @open_gerrit_change ) =
+  sort { $a->{number} <=> $b->{number} }
+  grep { $_->{open} }
+  map { $json->decode( $_ ) }
+  @gerrit_output;
+
+foreach my $ch ( @open_gerrit_change ) {
+  next unless $ch->{status} eq 'DRAFT';
+  my $ps = $ch->{currentPatchSet};
+  my $bname = $ch->{branch};
+  my $pname = $ch->{project};
+  my $cmd = qq{ssh -p ${gerrit_port} ${gerrit_host} gerrit review -p ${pname} --publish $ch->{number},$ps->{number}};
+  print STDERR $cmd, $/;
+  system($cmd);
+}
+
+
